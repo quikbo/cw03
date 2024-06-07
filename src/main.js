e470d85ba4a185528cf77bc41bf6e247620b3119
williamb301 <wboudy1@jhu.edu>
2024-06-07 15:23:02 -0400

    Initial commit: Set up project with Vite, Tailwind CSS, and Prettier

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import "../style.css";
+
+const app = document.getElementById("app");
+
+app.innerHTML = `
+    <div class="flex items-center justify-center h-screen">
+        <h1 class="text-5xl">Hello World!</h1>
+    </div>
+`;
